1216fba9a9dab71320c5130104857af0c346cf41
映銓 蘇 <sulli.he@outlook.com>
2023-11-03 19:22:28 +0800

    first commit

---
+++ SqlFormat/Helper/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace SqlFormat.Helper;
+
+public static class FileHelper
+{
+    public static async Task FileWriteAsync(string filePath, string text)
+    {
+        await File.WriteAllTextAsync(filePath, text);
+        await Console.Out.WriteLineAsync("檔案寫入完畢\n");
+    }
+
+    public static async Task FileReadAsync(string filePath)
+    {
+        string text = await File.ReadAllTextAsync(filePath);
+        await Console.Out.WriteLineAsync(text);
+    }
+}
