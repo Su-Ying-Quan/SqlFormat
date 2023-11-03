7f728ba5213095debb999633b3f3a15255b23bdf
littletrainee <arechery.a1111@gmail.com>
2023-11-03 22:30:57 +0800

    完成簡單的雛形

---
+++ SqlFormat/Setting.cs
@@ -0,0 +1,16 @@
+namespace SqlFormat
+{
+    public class Setting
+    {
+
+        /// <summary>
+        /// 縮排控制
+        /// </summary>
+        public int Indent { get; set; }
+        /// <summary>
+        /// FROM後面的Table是否要換行
+        /// </summary>
+        public bool TableChangeLine { get; set; }
+
+    }
+}
--- SqlFormat/SqlFormat.cs
+++ SqlFormat/SqlFormat.cs
@@ -1,28 +1,60 @@
+
+using System.Runtime.CompilerServices;
+
 namespace SqlFormat
 {
     public class SqlFormat
     {
-        private string ConfigFile { get; set; } = @".\config.ini";
+        private IniManager IniManager { get; set; }
+        private Setting Setting { get; set; }
+        private string TargetFilePath { get; set; }
+        private List<string> KeyWords { get; set; }
+
         public SqlFormat(string[] args)
         {
+            this.KeyWords = new List<string>(){
+                "select",
+                "from"
+            };
             try
             {
-                // Load Config File 
-                if (File.Exists(this.ConfigFile))
+                this.IniManager = new IniManager(out Setting s);
+                this.Setting = s;
+                this.TargetFilePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), args[0]);
+                if (File.Exists(this.TargetFilePath))
                 {
-                    var config = File.ReadAllLines(@".\config.ini");
-                }
-                else
-                {
-                    File.Create(this.ConfigFile);
+                    string[] f = File.ReadAllLines(this.TargetFilePath);
+                    List<string> li = f.ToList();
+                    //確認分號
+                    for (int i = 0; i < li.Count; i++)
+                    {
+                        li[i] = li[i].Trim();
+                        if (this.KeyWords.Contains(li[i].ToLower()))
+                        {
+                            li[i] = li[i].ToUpper();
+                        }
+                        else
+                        {
+                            li[i] = this.Space() + li[i];
+                        }
+                    }
+                    File.WriteAllText(this.TargetFilePath, string.Empty);
+                    File.WriteAllLines(this.TargetFilePath, li.ToArray());
                 }
             }
-            catch (FileNotFoundException)
+            catch (FileNotFoundException fe)
             {
-
+                Console.WriteLine(fe);
             }
-
-
+        }
+        private string Space()
+        {
+            string s = "";
+            for (var _ = 0; _ < this.Setting.Indent; _++)
+            {
+                s += " ";
+            }
+            return s;
         }
     }
 }
