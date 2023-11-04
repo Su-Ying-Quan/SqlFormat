49dbbf4b4db9fac080d1b07cc8da1a45f26bfe82
littletrainee <arechery.a1111@gmail.com>
2023-11-04 10:25:47 +0800

    驗證單行格式

--- SqlFormat/SqlFormat.cs
+++ SqlFormat/SqlFormat.cs
@@ -23,23 +23,15 @@ namespace SqlFormat
                 this.TargetFilePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), args[0]);
                 if (File.Exists(this.TargetFilePath))
                 {
+                    // TODO 是否需要只讀一部份文字
                     string[] f = File.ReadAllLines(this.TargetFilePath);
-                    List<string> li = f.ToList();
-                    //確認分號
-                    for (int i = 0; i < li.Count; i++)
-                    {
-                        li[i] = li[i].Trim();
-                        if (this.KeyWords.Contains(li[i].ToLower()))
-                        {
-                            li[i] = li[i].ToUpper();
-                        }
-                        else
-                        {
-                            li[i] = this.Space() + li[i];
-                        }
-                    }
-                    File.WriteAllText(this.TargetFilePath, string.Empty);
-                    File.WriteAllLines(this.TargetFilePath, li.ToArray());
+                    List<string> data = f.ToList();
+                    // this.FourLine(li);
+                    this.OneLine(ref data);
+                    data.Remove("");
+                    this.TargetFormat(data);
+                    // File.WriteAllText(this.TargetFilePath, string.Empty);
+                    File.WriteAllLines(this.TargetFilePath, data.ToArray());
                 }
             }
             catch (FileNotFoundException fe)
@@ -47,6 +39,28 @@ namespace SqlFormat
                 Console.WriteLine(fe);
             }
         }
+        private void OneLine(ref List<string> li)
+        {
+            li = li[0].Split(' ').ToList();
+        }
+
+        private void TargetFormat(List<string> li)
+        {
+            //確認分號
+            for (int i = 0; i < li.Count; i++)
+            {
+                li[i] = li[i].Trim();
+                if (this.KeyWords.Contains(li[i].ToLower()))
+                {
+                    li[i] = li[i].ToUpper();
+                }
+                else
+                {
+                    li[i] = this.Space() + li[i];
+                }
+            }
+        }
+
         private string Space()
         {
             string s = "";
