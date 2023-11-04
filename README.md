2f43564aafd367903b231eb784f65ed7e7026f65
littletrainee <arechery.a1111@gmail.com>
2023-11-04 22:22:41 +0800

    完成初版的原始檔內容轉內自資料格式

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# SqlFormat
+# SQL Format 大綱
 
-# 各個變數或關鍵字之間必須要有空格
+## 項目分配
 
-# TargetFormat -> Sulli
+> Sulli &rarr; TargetFormat
 
-# SourceFormt -> Littletrainee
+> Littletrainee &rarr; SourceFormt
+<br>
+
+# Ini格式規範
+I. 初級範本
+``` SQL
+SELECT * FROM Employee;
+```
+1. 各個 **[變數]** 或 **[關鍵字]** 之間必須要有空格。
+2. 暫定 **[縮格]** 為 **[4]** 格
+3. FROM Table 部分 **[不換行]**
--- SqlFormat/SqlFormat.cs
+++ SqlFormat/SqlFormat.cs
@@ -2,10 +2,17 @@ namespace SqlFormat;
 
 public class SqlFormat
 {
+    /// <summary>
+    /// Ini黨的控制物件
+    /// </summary>
     private IniManager IniManager { get; set; }
+    /// <summary>
+    /// User需要的目標格式持有物件
+    /// </summary>
     private Setting Setting { get; set; }
     private string TargetFilePath { get; set; }
     private List<string> KeyWords { get; set; }
+    private List<string> Data { get; set; }
 
     public SqlFormat(string[] args)
     {
@@ -20,40 +27,71 @@ public class SqlFormat
             this.TargetFilePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), args[0]);
             if (File.Exists(this.TargetFilePath))
             {
-                // TODO 是否需要只讀一部份文字
-                string[] f = File.ReadAllLines(this.TargetFilePath);
-                List<string> data = f.ToList();
-                // this.FourLine(li);
-                this.OneLine(ref data);
-                data.Remove("");
-                this.TargetFormat(data);
-                // File.WriteAllText(this.TargetFilePath, string.Empty);
-                File.WriteAllLines(this.TargetFilePath, data.ToArray());
+                // 讀取檔案後轉換為內定的資料格式 
+                this.ConvertRawContent(File.ReadAllLines(this.TargetFilePath));
+
+                // 將內定資料格式轉換為User想要的目標格式
+                this.TargetFormat();
+
+                // 將目標格式陣列寫進檔案內
+                File.WriteAllLines(this.TargetFilePath, this.Data.ToArray());
             }
         }
         catch (FileNotFoundException fe)
         {
             Console.WriteLine(fe);
         }
-    }
-    private void OneLine(ref List<string> li)
-    {
-        li = li[0].Split(' ').ToList();
-    }
-
-    private void TargetFormat(List<string> li)
-    {
-        //確認分號
-        for (int i = 0; i < li.Count; i++)
+        catch (ArgumentOutOfRangeException ae)
         {
-            li[i] = li[i].Trim();
-            if (this.KeyWords.Contains(li[i].ToLower()))
+            Console.WriteLine(ae);
+        }
+    }
+    /// <summary>
+    /// 轉換原始檔案的內容為內定的資料格式
+    /// </summary>
+    /// <param name="source">從檔案讀取出來的字串陣列</param>
+    public void ConvertRawContent(string[] source)
+    {
+        // 對原先的字串陣列當中每個元素做調整添加空格的部分並處理分號為獨立元素
+        for (int i = 0; i < source.Length; i++)
+        {
+            if (i == source.Length - 1)
             {
-                li[i] = li[i].ToUpper();
+                if (source[i].Contains(';'))
+                {
+                    source[i] = string.Concat(source[i].AsSpan(0, source[i].Length - 2), " ; ");
+                }
+                else
+                {
+                    source[i] += " ; ";
+                }
             }
             else
             {
-                li[i] = this.Space() + li[i];
+                source[i] += " ";
+            }
+        }
+
+        // 合併字串陣列當中每個添加空白間格的元素為一個字串，並分割為陣列後轉為List
+        this.Data = string.Join(" ", source.Select(v => v)).Split(" ").ToList();
+
+        // 移除所有空的元素
+        this.Data.RemoveAll(e => e == "");
+    }
+
+    private void TargetFormat()
+    {
+        //確認分號
+        for (int i = 0; i < this.Data.Count; i++)
+        {
+            this.Data[i] = this.Data[i].Trim();
+            if (this.KeyWords.Contains(this.Data[i].ToLower()))
+            {
+                this.Data[i] = this.Data[i].ToUpper();
+            }
+            else
+            {
+                this.Data[i] = this.Space() + this.Data[i];
             }
         }
     }
